c381304e50ade4c63e77b4cd284b61e1dce96abf
unknown <matheus.pereira4025@hotmail.com>
2020-02-19 17:32:24 -0300

    Update

--- models/Customer.js
+++ models/Customer.js
@@ -1,5 +1,16 @@
 const mongoose = require('mongoose');
 const timestamp = require('mongoose-timestamp');
 
-const
+const CustomerSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    name: {
+        email: String,
+        required: true,
+        trim: true
+    },
+});
 
